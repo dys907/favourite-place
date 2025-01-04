abc62be7a73b089e21b26729beeb2751fc89816f
Dylan Sung <dys907@gmail.com>
2025-01-04 11:46:40 -0800

    added basic form for adding places

--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -1,9 +1,47 @@
-import { Text, View } from "react-native"
+import { useState } from "react";
+import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
+import { Colours } from "../../constants/colours";
 
-const PlaceForm = (params) => {
-    return <View>
-        <Text>The place form</Text>
-    </View>
-}
+const PlaceForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
 
-export default PlaceForm
+  const changeTitleHandler = (enteredText) => {
+    setEnteredTitle(enteredText);
+  };
+
+  return (
+    <ScrollView style={styles.form}>
+      <View>
+        <Text style={styles.label}>Title</Text>
+        <TextInput
+          onChangeText={changeTitleHandler}
+          value={enteredTitle}
+          style={styles.input}
+        />
+      </View>
+    </ScrollView>
+  );
+};
+
+export default PlaceForm;
+
+const styles = StyleSheet.create({
+  form: {
+    flex: 1,
+    padding: 24,
+  },
+  label: {
+    fontWeight: "bold",
+    marginBottom: 4,
+    color: Colours.primary500,
+  },
+  input: {
+    marginVertical: 8,
+    paddingHorizontal: 4,
+    paddingVertical: 8,
+    fontSize: 16,
+    borderBottomColor: Colours.primary700,
+    borderBottomWidth: 2,
+    backgroundColor: Colours.primary100,
+  },
+});
